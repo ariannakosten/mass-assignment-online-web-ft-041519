3693c3e74977599403a650489d5f60486faba30e
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-05 23:29:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,11 @@ class Person
   
   def initialize(attributes)
       attributes.each do |key, value| 
-        self.send(("#{key}="), value)}
+        self.send(("#{key}="), value)
+    end
   end
 end
-end
 
+
+
+ 
